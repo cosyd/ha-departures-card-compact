c7dfc088cf0563ec0cd4ab3cf8f84fd298d81a70
Simon Rehbach <srehbach@gmail.com>
2025-12-28 23:21:19 +0100

    Fix naming #2

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ declare global {
   }
 }
 
-customElements.define("ha-departures-card-compact", DeparturesCard);
+customElements.define("departures-card-compact", DeparturesCard);
 
 
 window.customCards = window.customCards || [];
 window.customCards.push({
-  type: "ha-departures-card-compact",
-  name: "Departures card compact",
+  type: "departures-card-compact",
+  name: "Departures Card Compact",
   description: "Show public transport departures",
 });
